e1d34cf1e594b3f12279bf90d33d0843ed4e35ea
imstarving31 <2iamduy@gmail.com>
2025-04-10 22:17:57 +0700

    Update LoginActivity.kt

--- app/src/main/java/android/example/com/baristabit/LoginActivity.kt
+++ app/src/main/java/android/example/com/baristabit/LoginActivity.kt
@@ -1,11 +1,17 @@
 package android.example.com.baristabit
 
+import android.example.com.baristabit.databinding.ActivityMainBinding
+import android.example.com.baristabit.databinding.LoginBinding
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: LoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.login)  // Giao diện bạn gửi ở trên
+
+        binding = LoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
