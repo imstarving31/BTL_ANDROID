fa3561d6c518ddc43cc346243842d02a6044515a
lephuc1306 <lehongphuc13062004@gmail.com>
2025-04-11 00:20:28 +0700

    .

--- app/src/main/java/android/example/com/baristabit/models/Order2.kt
+++ app/src/main/java/android/example/com/baristabit/models/Order2.kt
@@ -4,13 +4,8 @@ data class Order2(
     val id: String,
     val customerName: String,
     val orderTime: String,
-    val items: List<OrderItem>,
+    val items: List<String>,
     val orderType: String,
     val doneTime: String
 )
 
-data class OrderItem(
-    val name: String,
-    val quantity: Int,
-    val price: Double? = null
-)
